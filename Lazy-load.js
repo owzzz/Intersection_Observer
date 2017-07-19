9e9451ecf2adecfb7e1c80fe341f4211f1d84555
Owain Llewellyn <owain.llew@gmail.com>
2017-07-19 09:44:18 -0400

    Create Lazy-load.js

---
+++ Lazy-load.js
@@ -0,0 +1,26 @@
+class LazyLoad {
+
+	constructor() {
+
+		let options = {
+	    	rootMargin: '0px',
+	    	threshold: [0]
+		};
+
+		this._observer = new IntersectionObserver((entries) => {
+			entries.forEach(function(img, observer) {
+				if(img.target.contains('er-image--loaded')) {
+					return;
+				}
+				if(img.isIntersecting) {
+					img.target.classList.add('er-image--loaded');
+					img.target.src = img.target.getAttribute('data-src');
+				}
+			});
+		}, options);
+
+	    let collection = [ ...document.querySelectorAll('[data-src]') ];
+
+	    collection.forEach(img => this._observer.observe(img));
+	}
+}
